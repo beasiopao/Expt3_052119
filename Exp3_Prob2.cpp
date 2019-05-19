41e035b8766e302e5c443731096c41c6aba3c65e
Beatriz <beatrizmarie.bagalay.eng@ust.edu.ph>
2019-05-19 16:33:10 +0800

    2

---
+++ Exp3_Prob2.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	double ProvA[7], ProvB[7], ProvC[7];
+
+	//INPUT of TEMP
+	cout << "===Enter all temperatures in Province A for this week===" << endl;
+	for (x=0; x<7; x++)
+	{
+		cout << "Temperature at Province A, Day " << x+1 << ": ";
+		cin >> ProvA[x];
+	}
+
+	cout << "\n\n===Enter all temperatures in Province B for this week===" << endl;
+	for (x=0; x<7; x++)
+	{
+		cout << "Temperature at Province B, Day " << x+1 << ": ";
+		cin >> ProvB[x];
+	}
+
+	cout << "\n\n===Enter all temperatures in Province A for this week===" << endl;
+	for (x=0; x<7; x++)
+	{
+		cout << "Temperature at Province C, Day " << x+1 << ": ";
+		cin >> ProvC[x];
+	}
+
+
+
+	//DISPLAY of TEMP
+	
+	cout << "\n\n\n\===Displaying Values for Temperatures in Each Province===" << "\n";
+	cout << "Temperatures in Province A for this week:" << "\n";
+	for (x=0; x<7; x++)
+	{
+	cout << "Temperature at Province A, Day " << x+1 << ": " << ProvA[x] << endl;
+	}
+	
+	cout << "\nTemperatures in Province A for this week:" << "\n";
+	for (x=0; x<7; x++)
+	{
+	cout << "Temperature at Province B, Day " << x+1 << ": " << ProvB[x] << endl;
+	}
+	
+	cout << "\nTemperatures in Province A for this week:" << "\n";
+	for (x=0; x<7; x++)
+	{
+	cout << "Temperature for Province C, Day " << x+1 << ":" << ProvC[x] << endl;
+	}
+
+	getch();
+	return 0;
+}
